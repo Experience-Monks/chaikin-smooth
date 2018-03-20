1384304e0dd673aeb1c3735fb46e4a77ad5dfc5a
Marc Jansen <jansen@terrestris.de>
2018-03-20 11:09:17 +0100

    Add support for closed rings
    See also http://graphics.cs.ucdavis.edu/education/CAGDNotes/Chaikins-Algorithm/Chaikins-Algorithm.html

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ module.exports = function(input, output) {
     if (!Array.isArray(output))
         output = []
 
-    if (input.length>0)
+    var inputLen = input.length;
+    var lastIdx = inputLen - 1;
+
+    var isClosedRing = inputLen > 2
+        && input[0][0] === input[lastIdx][0]
+        && input[0][1] === input[lastIdx][1];
+
+    // Push first pair if input is not a closed ring
+    if (inputLen > 0 && !isClosedRing) {
         output.push(copy([0, 0], input[0]))
-    for (var i=0; i<input.length-1; i++) {
+    }
+    for (var i = 0; i < lastIdx; i++) {
         var p0 = input[i]
         var p1 = input[i+1]
+
+        if (isClosedRing) {
+            var currentIdx = i % lastIdx;
+            var nextIdx = (i+1) % lastIdx;
+            p0 = input[currentIdx];
+            p1 = input[nextIdx];
+        }
+
         var p0x = p0[0],
             p0y = p0[1],
             p1x = p1[0],
@@ -19,7 +36,14 @@ module.exports = function(input, output) {
         output.push(Q)
         output.push(R)
     }
-    if (input.length > 1)
-        output.push(copy([0, 0], input[ input.length-1 ]))
+    // Push last pair if input is not a closed ring
+    if (inputLen > 1 && !isClosedRing) {
+        output.push(copy([0, 0], input[ lastIdx ]))
+    }
+    // Close originally passed rings once done
+    if (inputLen > 1 && isClosedRing) {
+        output.push(copy([0, 0], output[ 0 ]))
+    }
+
     return output
 }
